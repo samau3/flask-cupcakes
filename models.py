e1847a1f8317281e2831432e107bf94b3b048b6a
samau3 <samau415@gmail.com>
2021-09-20 12:20:42 -0700

    created cupcake model and basic listing, getting, and creating cupcake routes

--- models.py
+++ models.py
@@ -1,1 +1,34 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Cupcake(db.Model):
+    """Cupcake"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.Text, nullable=False,
+                      default="https://tinyurl.com/demo-cupcake")
+
+    def serialize(self):
+        """Serialize to dictionary"""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+        }
+
+
+def connect_db(app):
+    """Models for Blogly."""
+    db.app = app
+    db.init_app(app)
